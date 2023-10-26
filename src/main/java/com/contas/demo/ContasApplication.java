75280e02d841a6800ffeb24ebffdd3dd6c694890
Julio Donofreo <julio.bibico@hotmail.com>
2023-10-26 12:43:52 -0300

    Criado projeto e feita conexão com banco de dados

---
+++ src/main/java/com/contas/demo/ContasApplication.java
@@ -0,0 +1,13 @@
+package com.contas.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contas/demo/ContasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contas.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
